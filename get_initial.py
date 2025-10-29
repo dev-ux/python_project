8965af64d22e6b257bdc2591e19acdd00c770864
Kassi_0958 <marvinjunior0958@gmail.com>
2025-10-30 00:25:16 +0100

    Demo: Parameterized Functions

--- get_initial.py
+++ get_initial.py
@@ -1,18 +1,34 @@
-def get_initial(name):
-    initial = name[0:1]
+# def get_initial(name):
+#     initial = name[0:1]
+#     return initial
+
+
+
+# first_name = input("Enter your first name: ")
+# first_name_initial = get_initial(first_name)
+
+# middle_name = input("Enter your middle name: ")
+# middle_name_initial = get_initial(middle_name)
+
+# last_name = input("Enter your last name: ")
+# last_name_initial = get_initial(last_name)
+
+
+# print('Your initials are: ' + first_name_initial \
+#     + middle_name_initial + last_name_initial)
+
+
+def get_initial(name, force_uppercase):
+    if force_uppercase:
+        initial = name[0:1].upper()
+    else:
+        initial = name[0:1]
     return initial
 
 
 
 first_name = input("Enter your first name: ")
-first_name_initial = get_initial(first_name)
-
-middle_name = input("Enter your middle name: ")
-middle_name_initial = get_initial(middle_name)
-
-last_name = input("Enter your last name: ")
-last_name_initial = get_initial(last_name)
+first_name_initial = get_initial(first_name, False)
 
 
-print('Your initials are: ' + first_name_initial \
-    + middle_name_initial + last_name_initial)
+print('Your initials are: ' + first_name_initial)
